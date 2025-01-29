d295003f4b34afe920cf0789d403f15d2808b153
imramesh222 <imrameshrawat@gmail.com>
2025-01-29 23:28:07 +0545

    product add update delete

--- controller/productController.js
+++ controller/productController.js
@@ -1,5 +1,5 @@
 const ProductModel=require("../models/productModel.js")
-
+const fs=require('fs')
 //add new product
 
 exports.addProduct=async(req,res)=>{
@@ -20,23 +20,65 @@ exports.addProduct=async(req,res)=>{
   }
   res.send(productToAdd)
 }
+//Get all products
+exports.getAllProducts=async (req,res)=>{
+  let products=await ProductModel.find().populate('category')
+  if(!products){
+    return res.status(400).json({error:"Something went worng"})
+  }
+  res.send(products)
+}
+
+//get products details
+exports.getProductDetails=async (req,res)=>{
+  let product=await ProductModel.find().populate('category',"category_name")
+  if(!product){
+    return res.status(400).json({error:"Something went worng"})
+  }
+  res.send(product)
+}
+//get products by category
+exports.getProductsByCategory=async (req,res)=>{
+  let products=await ProductModel.find({category:req.params.categoryId})
+  if(!products){
+    return res.status(400).json({error:"Something went worng"})
+  }
+  res.send(products)
+}
+
+//update product
+exports.updateProduct=async (req,res)=>{
+  let productToUpdate=await ProductModel.findById(req.params.id)
+  if(!productToUpdate){
+    return res.status(400).json({error:"Something went wrong"})
+  }
+  if(req.file){
+    fs.unlinkSync(productToUpdate.product_image)
+    productToUpdate.product_image=req.file.path
+  }
+  let{product_title,product_price,product_description,count_in_stock,category,rating}=req.body
+
+  productToUpdate.product_title = product_title? product_title : productToUpdate.product_title
+  productToUpdate.product_price = product_price? product_price : productToUpdate.product_price
+  productToUpdate.product_description = product_description? product_description : productToUpdate.product_description
+  productToUpdate.count_in_stock = count_in_stock? count_in_stock : productToUpdate.count_in_stock
+  productToUpdate.category = category? category : productToUpdate.category
+  productToUpdate.rating = rating? rating : productToUpdate.rating
+
+  productToUpdate=await productToUpdate.save()
+
+  if(!productToUpdate){
+    return res.status(400).json({error:"Something went wrong"})
+  }
+  res.send(productToUpdate)
+
+}
+
+
+
+
+
   // delete product
-// exports.deleteProduct=(req,res)=>{
-//   ProductModel.findByIdAndDelete(req.params.id)
-//   .then(deletdProduct=>{
-//     if(!deletdProduct){
-
-//     return res.status(400).json({error:"Product deleted successfully."})
-//     }
-//     else{
-//   res.send({message:"Product deleted successfully"})
-//     }
-//   })
-//   .catch(error=>res.status(500).send(error))
-  
-// }
-// }
-
 exports.deleteProduct = (req,res) =>{
   ProductModel.findByIdAndDelete(req.params.id)
   .then(deletedProduct=>{
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,17 @@
-const { addProduct, deleteProduct } = require("../controller/productController");
+const { addProduct, deleteProduct, getAllProducts, getProductDetails, getProductsByCategory, updateProduct } = require("../controller/productController");
 const { upload } = require("../middleware/fileUpload");
 
 
 const router=require("express").Router();
 
 router.post('/addproduct',upload.single('product_image'),addProduct)
+
+router.get('/getallproducts',getAllProducts)
+
+router.get('/getproductdetails/:id',getProductDetails)
+router.get('/getproductsbycategory/:categoryId',getProductsByCategory)
+
+router.put('/updateproduct/:id',upload.single('product_image'),updateProduct)
 router.delete('/deleteproduct/:id',deleteProduct)
 
 
